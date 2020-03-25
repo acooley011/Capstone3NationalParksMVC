29f3f2aa2a05eb90ebe68cfd7359e9722670212f
Archie Cooley <acooley011@gmail.com>
2020-03-25 19:51:54 -0400

    jdbc maybe done?

--- module-3/12-Capstone/java/src/main/java/com/techelevator/npgeek/JDBC/JDBCSurveyResultDAO.java
+++ module-3/12-Capstone/java/src/main/java/com/techelevator/npgeek/JDBC/JDBCSurveyResultDAO.java
@@ -1,13 +1,55 @@
 package com.techelevator.npgeek.JDBC;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+import com.techelevator.npgeek.SurveyResult;
 import com.techelevator.npgeek.SurveyResultDAO;
 
+@Component
 public class JDBCSurveyResultDAO implements SurveyResultDAO {
+	
+	private JdbcTemplate jdbcTemplate;
 
-	@Override
-	public void insertNewSurvey() {
-		// TODO Auto-generated method stub
-
+	@Autowired
+	public JDBCSurveyResultDAO(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 
+	@Override
+	public void insertNewSurvey(String parkCode, String emailAddress, String state, String activityLevel) {
+		String insertSurvey = "INSERT INTO survey_result (parkcode, emailaddress, state, activitylevel) "
+				+ "VALUES (?, ?, ?, ?)";
+		jdbcTemplate.update(insertSurvey, parkCode, emailAddress, state, activityLevel);
+		
+	}
+
+	@Override
+	public List<SurveyResult> allSurveys() {
+		List<SurveyResult> allSurveys = new ArrayList<>();
+		String sqlGetAllSurveys = "SELECT * FROM survey_result GROUP BY parkcode";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlGetAllSurveys);
+		
+		while(results.next()) {
+			SurveyResult nextSurvey = new SurveyResult();
+			nextSurvey.setSurveyId(results.getInt("surveyid"));
+			nextSurvey.setParkCode(results.getString("parkcode"));
+			nextSurvey.setEmailAddress(results.getString("emailaddress"));
+			nextSurvey.setState(results.getString("state"));
+			nextSurvey.setActivityLevel(results.getString("activitylevel"));
+			allSurveys.add(nextSurvey);
+			
+		}
+		
+		return allSurveys;
+	}
+
+	
 }
--- module-3/12-Capstone/java/src/main/java/com/techelevator/npgeek/SurveyResultDAO.java
+++ module-3/12-Capstone/java/src/main/java/com/techelevator/npgeek/SurveyResultDAO.java
@@ -1,6 +1,11 @@
 package com.techelevator.npgeek;
 
+import java.util.List;
+
 public interface SurveyResultDAO {
-	public void insertNewSurvey();
+	
+	public void insertNewSurvey(String parkCode, String emailAddress, String state, String activityLevel);
+	
+	public List<SurveyResult> allSurveys();
 
 }
