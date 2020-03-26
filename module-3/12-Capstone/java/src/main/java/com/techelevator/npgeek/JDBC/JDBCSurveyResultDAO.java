9b1e259711b2c3d083c3d7b080cf03f49367c922
Archie Cooley <acooley011@gmail.com>
2020-03-26 17:23:25 -0400

    womp

--- module-3/12-Capstone/java/src/main/java/com/techelevator/npgeek/JDBC/JDBCSurveyResultDAO.java
+++ module-3/12-Capstone/java/src/main/java/com/techelevator/npgeek/JDBC/JDBCSurveyResultDAO.java
@@ -1,8 +1,6 @@
 package com.techelevator.npgeek.JDBC;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.sql.DataSource;
@@ -13,13 +11,16 @@ import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
 import com.techelevator.npgeek.Park;
-import com.techelevator.npgeek.SurveyResult;
+import com.techelevator.npgeek.ParkDAO;
 import com.techelevator.npgeek.SurveyResultDAO;
 
 @Component
 public class JDBCSurveyResultDAO implements SurveyResultDAO {
 	
 	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	ParkDAO parkDAO;
 
 	@Autowired
 	public JDBCSurveyResultDAO(DataSource dataSource) {
@@ -34,38 +35,43 @@ public class JDBCSurveyResultDAO implements SurveyResultDAO {
 		
 	}
 
-	@Override
-	public List<SurveyResult> allSurveys() {
-		List<SurveyResult> allSurveys = new ArrayList<>();
-		String sqlGetAllSurveys = "SELECT * FROM survey_result GROUP BY parkcode, "
-				+ "surveyid";
-		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlGetAllSurveys);
-		
-		while(results.next()) {
-			SurveyResult nextSurvey = new SurveyResult();
-			nextSurvey.setSurveyId(results.getInt("surveyid"));
-			nextSurvey.setParkCode(results.getString("parkcode"));
-			nextSurvey.setEmailAddress(results.getString("emailaddress"));
-			nextSurvey.setState(results.getString("state"));
-			nextSurvey.setActivityLevel(results.getString("activitylevel"));
-			allSurveys.add(nextSurvey);
-			
-		}
-		
-		return allSurveys;
-	}
+//	@Override
+//	public List<SurveyResult> allSurveys() {
+//		List<SurveyResult> allSurveys = new ArrayList<>();
+//		String sqlGetAllSurveys = "SELECT * FROM survey_result GROUP BY parkcode, "
+//				+ "surveyid";
+//		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlGetAllSurveys);
+//		
+//		while(results.next()) {
+//			SurveyResult nextSurvey = new SurveyResult();
+//			nextSurvey.setSurveyId(results.getInt("surveyid"));
+//			nextSurvey.setParkCode(results.getString("parkcode"));
+//			nextSurvey.setEmailAddress(results.getString("emailaddress"));
+//			nextSurvey.setState(results.getString("state"));
+//			nextSurvey.setActivityLevel(results.getString("activitylevel"));
+//			allSurveys.add(nextSurvey);
+//			
+//		}
+//		
+//		return allSurveys;
+//	}
 
 	@Override
-	public Map<Park, Integer> favoriteParkCount(String parkCode) {
+	public Map<Park, Integer> favoriteParkCount() {
 		Map<Park, Integer> favoriteParkCount = new HashMap<>();
-		String sqlGetParkCount = "SELECT parkcode, COUNT(parkcode) FROM survey_result GROUP BY parkcode";
+		String sqlGetParkCount = "SELECT park.parkname, survey_result.parkcode, COUNT(survey_result.parkcode) FROM survey_result JOIN park ON park.parkcode = survey_result.parkcode GROUP BY survey_result.parkcode, park.parkname"; 
 		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlGetParkCount);
 		
 		while(results.next()) {
-			favoriteParkCount.put(key, value)
+			String parkName = results.getString("parkname");
+			Integer parkCount = results.getInt("count");
+			
+			Park park = parkDAO.getParkInfoFromName(parkName);
+			favoriteParkCount.put(park, parkCount);
 		}
 		
-		return null;
+		
+		return favoriteParkCount;
 	}
 
 	
--- module-3/12-Capstone/java/src/main/webapp/WEB-INF/jsp/favoriteParks.jsp
+++ module-3/12-Capstone/java/src/main/webapp/WEB-INF/jsp/favoriteParks.jsp
@@ -4,8 +4,11 @@
 <div class="body-content">
 
 	
-	<c:forEach items="${surveys}" var="survey">
-		<p>${survey.emailAddress}</p>
+	<c:forEach items="${favoriteParks}" var="survey">
+	<c:url value="/img/parks/${survey.key.parkCode.toLowerCase()}.jpg" var="parkPic" />
+		    	<img class="park-picture" src="${parkPic}">
+		<p>${survey.key.parkName}</p>
+		<p>${survey.value}</p>
 	</c:forEach>
 	
 </div>
